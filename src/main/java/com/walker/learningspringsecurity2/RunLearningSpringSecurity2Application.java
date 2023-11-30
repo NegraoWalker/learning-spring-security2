b5d82c51c846451704e3de4faa7d7432cbc4bec7
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-11-30 08:51:12 -0300

    Criação da aplicação

---
+++ src/main/java/com/walker/learningspringsecurity2/RunLearningSpringSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.walker.learningspringsecurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunLearningSpringSecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunLearningSpringSecurity2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/walker/learningspringsecurity2/LearningSpringSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.learningspringsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringSecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
