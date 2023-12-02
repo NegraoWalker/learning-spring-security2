4efd682474ac8c2e1833a28bc4afdd6e08109796
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-12-02 13:57:06 -0300

    Decodificação e resposta do token para o user realizar a autenticação - Versão Final

--- src/main/java/com/walker/learningspringsecurity2/security/TokenValidation.java
+++ src/main/java/com/walker/learningspringsecurity2/security/TokenValidation.java
@@ -1,8 +1,7 @@
 package com.walker.learningspringsecurity2.security;
 
 import com.walker.learningspringsecurity2.entity.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,6 +10,7 @@ import org.springframework.security.core.Authentication;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.PublicKey;
 import java.util.Collections;
 import java.util.Date;
 
@@ -33,7 +33,18 @@ public class TokenValidation {
         return new AuthToken(TOKEN_HEADER + tokenJWT);
     }
     public static Authentication decodetoken(HttpServletRequest httpServletRequest) throws Exception{ //Validação do acesso
-        if (httpServletRequest.getHeader("Authorization").equals("Bearer *token-walker-123456")){ //Passando o token que criei para validação
+        String jwtToken = httpServletRequest.getHeader("Authorization");
+        jwtToken = jwtToken.replace(TOKEN_HEADER,"");
+        Jws<Claims> claimsJws = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(TOKEN_KEY.getBytes(StandardCharsets.UTF_8))).build().parseSignedClaims(jwtToken);
+
+        String userLogin = claimsJws.getPayload().getSubject(); //Usuário
+        System.out.println(userLogin);
+        String sender = claimsJws.getPayload().getIssuer(); //Emissor
+        System.out.println(sender);
+        Date expirationTime = claimsJws.getPayload().getExpiration(); //Expiração do token
+        System.out.println(expirationTime);
+
+        if (userLogin.length() > 0 && sender.equals(EMISSOR) && expirationTime.after(new Date(System.currentTimeMillis()))){ //Passando o token que criei para validação
             return new UsernamePasswordAuthenticationToken("user",null, Collections.emptyList()); //Retornando que o usuário está autenticado
         }
         return null;
